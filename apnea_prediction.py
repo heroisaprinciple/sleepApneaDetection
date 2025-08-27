0956eeae727c94dcb81232ff5c977c4030f91a09
Arina <valcat552@gmail.com>
2025-08-27 18:11:53 +0100

    add and refactor prediction and comparison files

---
+++ apnea_prediction.py
@@ -0,0 +1,45 @@
+import numpy as np
+import tensorflow as tf
+
+DIR = "fetched_recordings"
+PATIENT_ID = "00001463-100507"
+EVENT = "apnea" # or non_apnea
+SEGMENT = "seg_252"
+MODEL_PATH = "models/best_model.keras"
+
+def test_audio(filename, model):
+    path = f"{DIR}/{PATIENT_ID}/{EVENT}/{SEGMENT}/{filename}"
+    spec = preprocess_spec(path)
+    prob, label = predict(model, spec)
+    print(f"{filename}: Probability = {prob:.4f} -> Class: {label}")
+
+def predict(model, spec):
+    prob = model.predict(spec)[0][0]
+    # if prob is > 0.5, then the event is apneic
+    label = "Apnea" if prob > 0.5 else "Normal"
+    return prob, label
+
+def load_model(model_path=MODEL_PATH):
+    return tf.keras.models.load_model(f"{model_path}")
+
+def preprocess_spec(path):
+    spec = np.load(path)
+    spec = (spec - np.mean(spec)) / (np.std(spec) + 1e-8)
+    spec = np.expand_dims(spec, axis=-1)  # (64, 313, 1)
+    spec = np.expand_dims(spec, axis=0)  # (1, 64, 313, 1)
+    spec = spec.astype(np.float32)
+    return spec
+
+if __name__ == "__main__":
+    model = load_model()
+
+    # test original segment
+    test_audio("original_spectrogram.npy", model)
+    # test raw segment
+    test_audio("recorded_spectrogram_raw.npy", model)
+    # test clean segment
+    test_audio("recorded_spectrogram_clean.npy", model)
+
+
+
+
--- fetch_data.py
+++ fetch_data.py
@@ -6,12 +6,13 @@ from pathlib import Path
 # The credentials are properly secured.
 # Thus, exposing a bucket name and key patterns has no security risk.
 
-PATIENT_ID = "00001476-100507"
+PATIENT_ID = "00001486-100507"
 BUCKET = "apnearecordings"
-SEGMENT = "seg_009"
-KEY = f"{PATIENT_ID}/2025/08/20/13/49/28/recorded_clean.wav"
+SEGMENT = "seg_000"
+KEY = f"{PATIENT_ID}/2025/08/21/15/20/04/recorded_raw.wav"
+EVENT = "non_apnea" # could be apnea
 
-out_path = Path("fetched_recordings") / PATIENT_ID / SEGMENT / "recorded_segment_clean.wav"
+out_path = Path("fetched_recordings") / PATIENT_ID / EVENT / SEGMENT / "recorded_segment_raw.wav"
 out_path.parent.mkdir(parents=True, exist_ok=True)
 
 s3 = boto3.client("s3")
